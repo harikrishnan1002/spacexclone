7164feba35f1f98f6ac4a4db05ec1bee6b2de01f
Hari Krishnan <harikrishnanrg3@gmail.com>
2024-04-04 13:08:27 +0530

    first commit

---
+++ intex.js
@@ -0,0 +1,6 @@
+document.querySelector(".sidebar .toggle-btn").addEventListener("click",function(){
+    document.querySelector(".sidebar").classList.toggle("active")
+})
+// .addEventListener("click",function(){
+//     document.querySelector("sidebar").classList.toggle(" active");
+// });
